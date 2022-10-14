d904f88bd8ae17a6fea42770c7c81cf41007b5a5
Davyd Almeida <almeidadavyd@outlook.com>
2022-10-13 23:10:04 -0300

    criando os componentes lis e number

--- APS1/src/app/app.module.ts
+++ APS1/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListComponent } from './list/list.component';
+import { NumberComponent } from './number/number.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent,
+    NumberComponent
   ],
   imports: [
     BrowserModule,
